56c1bb55bd864904d12597e199fa60f3bb3791c9
Ghaith AlMasri <almasrighaith101@gmail.com>
2024-03-07 18:07:53 +0300

    finished hero

--- avocateca/app/components/hero/Hero.jsx
+++ avocateca/app/components/hero/Hero.jsx
@@ -63,9 +63,9 @@ const Hero = () => {
         </div>
 
         <div className="flex justify-center items-center text-center w-full gap-10 text-wrap flex-wrap">
-          <Card text={"Drept Penal"} iconPath={"/icon1.svg"} color="light-background-color" textColor={"text-color"}/>
-          <Card text={"Dreptul Familiei"} iconPath={"/icon2.svg"} color="light-background-color" textColor={"text-color"}/>
-          <Card text={"Drept Comercial"} iconPath={"/icon3.svg"} color="none" classes="bg-secondary-color-4" textColor={"white"}/>
+          <Card text={"Drept Penal"} iconPath={"/icon1.svg"} color="light-background-color" textColor={"text-color"} subText={"Apărare solidă pentru acuzații variate."} subTextColor={"second-text-color"}/>
+          <Card text={"Dreptul Familiei"} iconPath={"/icon2.svg"} color="light-background-color" textColor={"text-color"} subText={"Rezolvarea cu compasiune a litigiilor."} subTextColor={"second-text-color"}/>
+          <Card text={"Drept Comercial"} iconPath={"/icon3.svg"} color="none" classes="bg-secondary-color-4" textColor={"white"} subText={"Soluții personalizate pentru provocări de afaceri."} subTextColor={"white"}/>
         </div>
       </div>
     </motion.div>
