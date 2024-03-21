5c723f4dd5d3ec75363e6a55f76245079dd1af98
Ghaith AlMasri <almasrighaith101@gmail.com>
2024-03-22 02:53:20 +0300

    made animation smoother

--- avocateca/app/components/services/Services.jsx
+++ avocateca/app/components/services/Services.jsx
@@ -28,7 +28,7 @@ const Services = () => {
       >
         <motion.div
         initial={{ scale: 0, opacity: 0 }}
-        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {duration: 0.2, type: "spring", stiffness: 150, damping: 20} }}
+        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {type: "spring", stiffness: 80, damping: 10} }}
 
         >
           <Card text={"Fraudă sau induce în eroare"} iconPath={"/service1.jpg"} color="light-background-color" textColor={"text-color"} subText={"Deslușind complexitatea activităților frauduloase și a practicilor înșelătoare, suntem specializați în protejarea clienților de prejudicii financiare și de afectarea reputației prin intervenții juridice strategice și investigații amănunțite."} subTextColor={"second-text-color"} type={3}/>
@@ -36,14 +36,14 @@ const Services = () => {
 
         <motion.div
         initial={{ scale: 0, opacity: 0 }}
-        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {duration: 0.2, type: "spring", stiffness: 150, damping: 20, delay: 0.2} }}
+        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {type: "spring", stiffness: 80, damping: 10, delay: 0.2} }}
         >
           <Card text={"Cauțiuni sau mandate"} iconPath={"/service2.jpg"} color="light-background-color" textColor={"text-color"} subText={"Oferind asistență rapidă în parcurgerea complexității cauțiunilor și a mandatelor de cauțiune, expertiza noastră asigură rezolvarea în timp util și liniștea sufletească a clienților care se confruntă cu provocări juridice, facilitând revenirea lor rapidă la normalitate."} subTextColor={"second-text-color"} type={3}/>
         </motion.div>
 
         <motion.div
         initial={{ scale: 0, opacity: 0 }}
-        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {duration: 0.2, type: "spring", stiffness: 150, damping: 20, delay: 0.4} }}
+        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {type: "spring", stiffness: 80, damping: 10, delay: 0.4} }}
 
         >
           <Card text={"Infracțiuni federale privind drogurile"} iconPath={"/service3.jpg"} color="light-background-color" textColor={"text-color"} subText={"Cu o înțelegere profundă a legilor și reglementărilor federale, oferim strategii de apărare cuprinzătoare adaptate pentru a atenua consecințele grave ale acuzațiilor federale de droguri, protejând drepturile și viitorul clienților noștri."} subTextColor={"second-text-color"} type={3}/>
@@ -51,7 +51,7 @@ const Services = () => {
 
         <motion.div
         initial={{ scale: 0, opacity: 0 }}
-        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {duration: 0.2, type: "spring", stiffness: 150, damping: 20, delay: 0.6} }}
+        whileInView = {{ opacity:1, rotate: [-180, 0], scale: [0.5,1,1.2,1], transition: {type: "spring", stiffness: 80, damping: 10, delay: 0.6} }}
 
         >
           <Card text={"Infracțiuni legate de trafic"} iconPath={"/service4.jpg"} color="light-background-color" textColor={"text-color"} subText={"Combatting the serious implications of trafficking offenses, our specialized legal team employs a proactive approach to defense, crafting robust strategies to challenge allegations and protect the liberties of those accused."} subTextColor={"second-text-color"} type={3}/>
